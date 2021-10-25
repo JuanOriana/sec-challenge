bc79d4b34acc1666fd676ff181120ed45b8f8bfb
Juan <joriana@itba.edu.ar>
2021-10-25 13:06:02 -0300

    Main page with static items

---
+++ components/Card.js
@@ -0,0 +1,134 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { CartProvider, useCart } from "../contexts/CartContext";
+import Link from "next/link";
+
+const CardContainer = styled.div`
+  background: ${(props) => props.theme.colors.brand.platinum};
+  border-radius: 12px;
+  align-items: center;
+  box-shadow: 0px 1px 3px #555;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  color: white;
+  width: 320px;
+  margin: 8px;
+`;
+
+const CardData = styled.div`
+  border-radius: 12px;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CardImage = styled.img`
+  border: 3px solid ${(props) => props.theme.colors.brand.secondary};
+  height: 120px;
+  width: 120px;
+  border-radius: 50%;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const CardTitle = styled.h2`
+  color: ${(props) => props.theme.colors.brand.secondary};
+  margin-right: 10px;
+  text-transform: uppercase;
+  word-wrap: break-word;
+  font-size: 18px;
+  cursor: pointer;
+  &:hover {
+    color: #ff9999;
+    transition: ease-in-out 0.2s;
+  }
+`;
+
+const CardSize = styled.span`
+  color: #777;
+  font-size: 14px;
+  margin-left: 5px;
+  text-transform: lowercase;
+`;
+
+const CardPrice = styled.span`
+  color: black;
+  font-weight: 700;
+`;
+
+const CardUnits = styled.span`
+  color: #303033;
+  font-weight: 300;
+  margin-left: 3px;
+`;
+
+const ShoppingCartButton = styled.button`
+  align-self: end;
+  color: white;
+  background: ${(props) => props.theme.colors.brand.secondary};
+  font-weight: 700;
+  border: 0 solid white;
+  border-radius: 12px;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background: #ff8891;
+    transition: ease-in-out 0.2s;
+  }
+`;
+
+const ShoppingCartRemoveButton = styled(ShoppingCartButton)`
+  background: #ee4444;
+  &:hover {
+    background: #ee7777;
+  }
+`;
+
+const Card = ({ cardData }) => {
+  const {
+    product_id,
+    name,
+    total_price,
+    units_per_pack,
+    size,
+    discount_percentage,
+    image_url,
+  } = cardData;
+  const { cart, addItemToCart, removeItemFromCart } = useCart();
+
+  return (
+    <CardContainer>
+      <div style={{ display: "flex" }}>
+        <Link href={"/product/" + product_id}>
+          <CardImage src={image_url} alt={name} />
+        </Link>
+        <CardData>
+          <Link href={"/product/" + product_id}>
+            <CardTitle>
+              {name}
+              <CardSize>{size}ml</CardSize>
+            </CardTitle>
+          </Link>
+
+          <p>
+            <CardPrice>${total_price}</CardPrice>
+            <CardUnits> ({units_per_pack} unidades)</CardUnits>
+          </p>
+        </CardData>
+      </div>
+      {cart.indexOf(cardData) < 0 && (
+        <ShoppingCartButton onClick={() => addItemToCart(cardData)}>
+          Anadir al carrito
+        </ShoppingCartButton>
+      )}
+      {cart.indexOf(cardData) >= 0 && (
+        <ShoppingCartRemoveButton onClick={() => removeItemFromCart(cardData)}>
+          Sacar del carrito
+        </ShoppingCartRemoveButton>
+      )}
+    </CardContainer>
+  );
+};
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,117 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+import Card from "../components/Card";
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+`;
+
+const FilterGroup = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterSelect = styled.select`
+  color: white;
+  background: ${(props) => props.theme.colors.brand.primarydark};
+  padding: 10px;
+  border-radius: 12px;
+  font-weight: 500;
+  font-size: 16px;
+  margin-left: 10px;
+`;
+
+const data = [
+  {
+    product_id: 1,
+    name: "cervezaaa",
+    total_price: "600",
+    units_per_pack: 6,
+    discount_percentage: 20,
+    size: 333,
+    categories: ["vinos", "mas vendidos"],
+    image_url:
+      "https://cdn.shopify.com/s/files/1/0254/2947/5433/products/cerveza-andes-origen-rubia-473-siempreencasa_600x600.png?v=1629814628?nocache=0.763811395909223",
+  },
+  {
+    product_id: 2,
+    name: "otro",
+    total_price: "600",
+    units_per_pack: 6,
+    discount_percentage: 20,
+    size: 333,
+    categories: ["vinos", "mas vendidos"],
+    image_url:
+      "https://cdn.shopify.com/s/files/1/0254/2947/5433/products/cerveza-andes-origen-rubia-473-siempreencasa_600x600.png?v=1629814628?nocache=0.763811395909223",
+  },
+  {
+    product_id: 3,
+    name: "item 3",
+    total_price: "600",
+    units_per_pack: 6,
+    discount_percentage: 20,
+    size: 333,
+    categories: ["cervezas", "todos"],
+    image_url:
+      "https://cdn.shopify.com/s/files/1/0254/2947/5433/products/cerveza-andes-origen-rubia-473-siempreencasa_600x600.png?v=1629814628?nocache=0.763811395909223",
+  },
+  {
+    product_id: 4,
+    name: "vino",
+    total_price: "600",
+    units_per_pack: 6,
+    discount_percentage: 20,
+    size: 333,
+    categories: ["cervezas", "vinos"],
+    image_url:
+      "https://cdn.shopify.com/s/files/1/0254/2947/5433/products/cerveza-andes-origen-rubia-473-siempreencasa_600x600.png?v=1629814628?nocache=0.763811395909223",
+  },
+  {
+    product_id: 5,
+    name: "otro 2",
+    total_price: "600",
+    units_per_pack: 6,
+    categories: ["otros"],
+    discount_percentage: 20,
+    size: 333,
+    image_url:
+      "https://cdn.shopify.com/s/files/1/0254/2947/5433/products/cerveza-andes-origen-rubia-473-siempreencasa_600x600.png?v=1629814628?nocache=0.763811395909223",
+  },
+];
+
 export default function Home() {
-  return <div>prueba</div>;
+  const [filteredData, setFilteredData] = useState(data);
+
+  const filterData = (event) => {
+    if (event.target.value == "todos") {
+      setFilteredData(data);
+    } else {
+      setFilteredData(
+        data.filter((item) => item.categories.includes(event.target.value))
+      );
+    }
+  };
+  return (
+    <PageContainer>
+      <FilterGroup>
+        <p>Filtrar por</p>
+        <FilterSelect onChange={filterData}>
+          <option value="todos">Todos</option>
+          <option value="cervezas">Cervezas</option>
+          <option value="vinos">Vinos</option>
+          <option value="otros">Otros</option>
+        </FilterSelect>
+      </FilterGroup>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {filteredData.map((cardData, index) => (
+          <Card cardData={cardData} key={index} />
+        ))}
+      </div>
+    </PageContainer>
+  );
 }
